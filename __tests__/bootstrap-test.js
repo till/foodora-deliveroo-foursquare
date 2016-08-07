16998f091aafe0d7bd196551ca42edd8f6a2b9d6
till <till@php.net>
2016-08-07 23:15:05 +0200

    Refactor: group tests

--- __tests__/bootstrap-test.js
+++ __tests__/bootstrap-test.js
@@ -1,60 +1,64 @@
 jest.unmock('../lib/bootstrap');
 
 describe('Bootstrap', () => {
-    it('detects foodora', () => {
-        var Bootstrap = require('../lib/bootstrap'),
-            fake_location = {
-              hostname: 'foodora.de',
-              pathname: '/'
-            },
-            start = new Bootstrap(fake_location);
+    describe('foodora', () => {
+        it('is detected', () => {
+            var Bootstrap = require('../lib/bootstrap'),
+                fake_location = {
+                  hostname: 'foodora.de',
+                  pathname: '/'
+                },
+                start = new Bootstrap(fake_location);
 
-        expect(start.is_foodora()).toBeTruthy();
-    });
-
-    it('detects deliveroo', () => {
-      var Bootstrap = require('../lib/bootstrap'),
-          fake_location = {
-            hostname: 'deliveroo.de',
-            pathname: '/'
-          },
-          start = new Bootstrap(fake_location);
-
-      expect(start.is_foodora()).toBeFalsy();
-    });
-
-    it('foodora: extracts the right data', () => {
-        var Bootstrap = require('../lib/bootstrap'),
-            fake_location = {
-              hostname: 'foodora.de',
-              pathname: '/restaurants/lat/52.50065060000001/lng/13.417732099999967/plz/10999/city/Berlin/'
-            },
-            start = new Bootstrap(fake_location),
-            query_data = start.create_query();
-
-        ['city', 'zip', 'll'].forEach(function(property){
-            expect(query_data.hasOwnProperty(property)).toBeTruthy();
+            expect(start.is_foodora()).toBeTruthy();
         });
 
-        expect(query_data.city).toEqual('Berlin');
-        expect(query_data.zip).toEqual(10999);
-        expect(query_data.ll).toEqual('52.5,13.4');
+        it('extracts city, zip and ll from the URL', () => {
+            var Bootstrap = require('../lib/bootstrap'),
+                fake_location = {
+                  hostname: 'foodora.de',
+                  pathname: '/restaurants/lat/52.50065060000001/lng/13.417732099999967/plz/10999/city/Berlin/'
+                },
+                start = new Bootstrap(fake_location),
+                query_data = start.create_query();
+
+            ['city', 'zip', 'll'].forEach(function(property){
+                expect(query_data.hasOwnProperty(property)).toBeTruthy();
+            });
+
+            expect(query_data.city).toEqual('Berlin');
+            expect(query_data.zip).toEqual(10999);
+            expect(query_data.ll).toEqual('52.5,13.4');
+        });
     });
 
-    it('deliveroo: extracts the right data', () => {
-        var Bootstrap = require('../lib/bootstrap'),
-            fake_location = {
-              hostname: 'deliveroo.de',
-              pathname: '/de/restaurants/berlin/kreuzberg'
-            },
-            start = new Bootstrap(fake_location),
-            query_data = start.create_query();
+    describe('deliveroo', () => {
+        it('is detected', () => {
+            var Bootstrap = require('../lib/bootstrap'),
+                fake_location = {
+                  hostname: 'deliveroo.de',
+                  pathname: '/'
+                },
+                start = new Bootstrap(fake_location);
 
-        ['city', 'near'].forEach(function(property){
-            expect(query_data.hasOwnProperty(property)).toBeTruthy();
+            expect(start.is_foodora()).toBeFalsy();
         });
 
-        expect(query_data.city).toEqual('berlin');
-        expect(query_data.near).toEqual('kreuzberg');
+        it('extracts city and near from the URL', () => {
+            var Bootstrap = require('../lib/bootstrap'),
+                fake_location = {
+                  hostname: 'deliveroo.de',
+                  pathname: '/de/restaurants/berlin/kreuzberg'
+                },
+                start = new Bootstrap(fake_location),
+                query_data = start.create_query();
+
+            ['city', 'near'].forEach(function(property){
+                expect(query_data.hasOwnProperty(property)).toBeTruthy();
+            });
+
+            expect(query_data.city).toEqual('berlin');
+            expect(query_data.near).toEqual('kreuzberg');
+        });
     });
 });
